a96a0193fb2dfb6c84e15f1eccc339ce7b472019
maximus-output <maximus@themaximumoutput.com>
2026-06-08 18:40:46 -0400

    fix: replace UserWorkspaceEntity

--- packages/twenty-server/src/engine/core-modules/enterprise/enterprise.module.ts
+++ packages/twenty-server/src/engine/core-modules/enterprise/enterprise.module.ts
@@ -7,10 +7,12 @@ import { AppTokenEntity } from 'src/engine/core-modules/app-token/app-token.enti
 import { EnterpriseResolver } from 'src/engine/core-modules/enterprise/enterprise.resolver';
 import { EnterprisePlanService } from 'src/engine/core-modules/enterprise/services/enterprise-plan.service';
 import { TwentyConfigModule } from 'src/engine/core-modules/twenty-config/twenty-config.module';
+import { UserWorkspaceEntity } from 'src/engine/core-modules/user-workspace/user-workspace.entity';
+
 @Module({
   imports: [
     TwentyConfigModule,
-    TypeOrmModule.forFeature([AppTokenEntity]),
+    TypeOrmModule.forFeature([UserWorkspaceEntity, AppTokenEntity]),
   ],
   providers: [
     EnterprisePlanService,
